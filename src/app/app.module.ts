8c5398719b18c7041a5291426f51da6ee4d0f6ac
Emily Vogel <eav990@gmail.com>
2020-09-13 15:43:45 -0400

    Generate project bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SearchbarComponent } from './shared/searchbar/searchbar.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchbarComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
